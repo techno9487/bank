717403b16838763fdf35351bab663c0787d3b83b
Toby Dunn <tobythedude99@gmail.com>
2017-11-13 20:11:10 +0000

    added new test

--- tests.py
+++ tests.py
@@ -6,5 +6,9 @@ class PersonCase(unittest.TestCase):
         self.person = person.Person("John Smith","Password123",["1 somehwere lane"])
     def test_name(self):
         self.assertEqual(self.person.get_name(),"John Smith")
+    def test_update_name(self):
+        self.person.update_name("Sam")
+        self.assertEqual(self.person.name,"Sam")
+        self.person.name = "John Smith"
 
 unittest.main()
